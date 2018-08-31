69986f0d92e5d3f77bc37b0f0c1e58bc3365fd48
timmcallister <tim.mcallister84@gmail.com>
2018-08-31 10:32:41 -0500

    completed projects 3 and 4

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -6,9 +6,71 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here
+(GETKEY)
+@8192           // set index or counter
+D=A
+@counter        // store the index in memory
+M=D
+@offset         // set memory address offset
+M=0
+@KBD            // get keyboard input
+D=M
+@NOTPRESS
+D;JEQ
+(PRESS)         // key pressed set 1's
+@button
+M=-1
+@LOOP
+0;JMP
+(NOTPRESS)      // key not pressed set 0
+@button
+M=0
+(LOOP)
+@SCREEN         // load screen address in register
+D=A
+@offset         // load offset and add to screen address
+A=D+M
+D=A             // store address for use later
+@address
+M=D
+@button         // get button press
+D=M
+@address
+A=M
+M=D
+@offset         // load and increment offset
+M=M+1
+@counter        // load and decrement counter
+M=M-1
+D=M
+@LOOP
+D;JGT
+@GETKEY
+0;JMP
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,64 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Put your code here
+@R2         // clear R2
+M=0
+
+@R0         // if either term is 0, skip to end
+D=M
+@END
+D;JEQ
+@R1
+D=M
+@END
+D;JEQ
+
+
+(LOOP)
+@R0
+D=M
+@R2
+M=M+D
+@R1
+M=M-1
+D=M
+@END
+D;JEQ
+@LOOP
+0;JMP
+
+(END)
+0;JMP
+
+
+//@R0
+//D=M
+//@sum
+//M=D
+//@R2
+//M=0
+//(LOOP)
+//@R1
+//D=M-1
+//M=D
+//@DONE
+//D;JEQ
+//@R0
+//AD=M
+//D=D+A
+//@sum
+//M=D
+//@LOOP
+//0;JMP
+//(DONE)
+//@sum
+//D=M
+//@R2
+//M=D
+//(END)
+//@END
+//0;JMP
+
+
+
